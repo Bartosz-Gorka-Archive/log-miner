e2098094ae9e9d7139d6f8e49b16fac74a8b4ffd
Bartosz Górka <bartosz.gorka@interia.eu>
2019-11-10 09:00:52 +0100

    feat: Process Log Reader Changes: * Added `Activity` class with `activity name` * Added interface to force `contract based` actions * Added `ProcessLogReader` to:  - Use `Scanner` as reader  - Read whole file  - Remove duplicates  - Parse string `case log` to list of activities

---
+++ src/main/java/reader/ProcessLogReader.java
@@ -0,0 +1,38 @@
+package reader;
+
+import interfaces.ReadLogFile;
+import structures.Activity;
+
+import java.util.*;
+
+public class ProcessLogReader implements ReadLogFile {
+    public List<List<Activity>> readProcessLog(Scanner scanner) {
+        List<List<Activity>> activityInCases = new ArrayList<>();
+
+        for (String caseRecord : readWholeLog(scanner)) {
+            activityInCases.add(stringCaseToActivityList(caseRecord));
+        }
+
+        return activityInCases;
+    }
+
+    private Set<String> readWholeLog(Scanner scanner) {
+        Set<String> casesInLog = new HashSet<>();
+        while (scanner.hasNextLine()) {
+            casesInLog.add(scanner.nextLine());
+        }
+
+        return casesInLog;
+    }
+
+    private List<Activity> stringCaseToActivityList(String caseRecord) {
+        List<Activity> activityInCase = new LinkedList<>();
+
+        for (String name : caseRecord.split("(?!^)")) {
+            if (!name.trim().equals(""))
+                activityInCase.add(new Activity(name.trim()));
+        }
+
+        return activityInCase;
+    }
+}
