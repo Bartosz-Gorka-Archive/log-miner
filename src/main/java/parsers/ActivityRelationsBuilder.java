f36be916aee7fa0661e7410e572dac45b01ba72b
Bartosz Górka <bartosz.gorka@interia.eu>
2019-11-10 14:08:24 +0100

    feat: Relations between activities Changes: * Added ability to generate matrix relations of activities * Introduce custom `RelationType` enum to store relation operator (`->`, `||`, `#` or `<-`)

--- src/main/java/parsers/ActivityRelationsBuilder.java
+++ src/main/java/parsers/ActivityRelationsBuilder.java
@@ -2,7 +2,9 @@ package parsers;
 
 import structures.Activity;
 import structures.ActivityDirectSuccession;
+import structures.ActivityRelation;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -46,5 +48,53 @@ public class ActivityRelationsBuilder {
         }
 
         return uniqueActivities;
+    }
+
+    /**
+     * Generate a list of relations between activities.
+     * Each pair of activities has own relation stored in response
+     *
+     * @param cases Parsed log file as activities list
+     * @return Relations between activities in log file (an activities list)
+     */
+    public List<ActivityRelation> generateRelationsBetweenActivities(List<List<Activity>> cases) {
+        Set<Activity> uniqueActivities = this.generateUniqueActivities(cases);
+        List<ActivityRelation> relations = new ArrayList<>();
+        Set<ActivityDirectSuccession> directSuccessions = this.generateDirectSuccessions(cases);
+
+        for (Activity A : uniqueActivities) {
+            for (Activity B : uniqueActivities) {
+                boolean AB = directSuccessions.contains(new ActivityDirectSuccession(A, B));
+                boolean BA = directSuccessions.contains(new ActivityDirectSuccession(B, A));
+                ActivityRelation.RelationType type = this.determineRelation(AB, BA);
+
+                relations.add(new ActivityRelation(A, B, type));
+            }
+        }
+
+        return relations;
+    }
+
+    /**
+     * Decide which relation assign to pair of activities.
+     * <b>Causality</b>: a > b and not b > a
+     * <b>Reversed Causality</b>: b > a and not a > b
+     * <b>Parallel</b>: a > b and b > a
+     * <b>Choice</b>: not a > b and not b > a
+     *
+     * @param AB Boolean value with check a > b
+     * @param BA Boolean value with check b > a
+     * @return Relation type
+     */
+    private ActivityRelation.RelationType determineRelation(boolean AB, boolean BA) {
+        if (AB && !BA) {
+            return ActivityRelation.RelationType.CAUSALITY;
+        } else if (!AB && BA) {
+            return ActivityRelation.RelationType.REVERSED_CAUSALITY;
+        } else if (AB && BA) {
+            return ActivityRelation.RelationType.PARALLEL;
+        } else {
+            return ActivityRelation.RelationType.CHOICE;
+        }
     }
 }
--- src/test/java/parsers/ActivityRelationsBuilderTest.java
+++ src/test/java/parsers/ActivityRelationsBuilderTest.java
@@ -4,12 +4,15 @@ import org.junit.jupiter.api.Test;
 import reader.ProcessLogReader;
 import structures.Activity;
 import structures.ActivityDirectSuccession;
+import structures.ActivityRelation;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static structures.ActivityRelation.RelationType.*;
 
 class ActivityRelationsBuilderTest {
     /**
@@ -81,5 +84,67 @@ class ActivityRelationsBuilderTest {
         assertTrue(response.contains(new ActivityDirectSuccession(A, K)));
         assertTrue(response.contains(new ActivityDirectSuccession(K, F)));
         assertFalse(response.contains(new ActivityDirectSuccession(A, F)));
+    }
+
+    /**
+     * Test relations between activities in log:
+     * 1. abcd
+     * 2. acbd
+     * 3. aed
+     * <p>
+     * We expect to receive
+     * ... a   b   c   d   e
+     * a   #   ->  ->  #   ->
+     * b   <-  #   ||  ->  #
+     * c   <-  ||  #   ->  #
+     * d   #   <-  <-  #   <-
+     * e   <-  #   #   ->  #
+     */
+    @Test
+    void checkRelationsBetweenActivities() {
+        Activity A = new Activity("a");
+        Activity B = new Activity("b");
+        Activity C = new Activity("c");
+        Activity D = new Activity("d");
+        Activity E = new Activity("e");
+        Scanner log = new Scanner("abcd\nacbd\naed");
+        List<List<Activity>> input = new ProcessLogReader().readProcessLog(log);
+
+        List<ActivityRelation> response = new ActivityRelationsBuilder().generateRelationsBetweenActivities(input);
+        Comparator<ActivityRelation> comparator = Comparator.comparing(relation -> relation.getPredecessor().activityName());
+        comparator = comparator.thenComparing(relation -> relation.getSuccessor().activityName());
+        response.sort(comparator);
+
+        assertEquals(5 * 5, response.size());
+
+        assertEquals(new ActivityRelation(A, A, CHOICE), response.get(0));
+        assertEquals(new ActivityRelation(A, B, CAUSALITY), response.get(1));
+        assertEquals(new ActivityRelation(A, C, CAUSALITY), response.get(2));
+        assertEquals(new ActivityRelation(A, D, CHOICE), response.get(3));
+        assertEquals(new ActivityRelation(A, E, CAUSALITY), response.get(4));
+
+        assertEquals(new ActivityRelation(B, A, REVERSED_CAUSALITY), response.get(5));
+        assertEquals(new ActivityRelation(B, B, CHOICE), response.get(6));
+        assertEquals(new ActivityRelation(B, C, PARALLEL), response.get(7));
+        assertEquals(new ActivityRelation(B, D, CAUSALITY), response.get(8));
+        assertEquals(new ActivityRelation(B, E, CHOICE), response.get(9));
+
+        assertEquals(new ActivityRelation(C, A, REVERSED_CAUSALITY), response.get(10));
+        assertEquals(new ActivityRelation(C, B, PARALLEL), response.get(11));
+        assertEquals(new ActivityRelation(C, C, CHOICE), response.get(12));
+        assertEquals(new ActivityRelation(C, D, CAUSALITY), response.get(13));
+        assertEquals(new ActivityRelation(C, E, CHOICE), response.get(14));
+
+        assertEquals(new ActivityRelation(D, A, CHOICE), response.get(15));
+        assertEquals(new ActivityRelation(D, B, REVERSED_CAUSALITY), response.get(16));
+        assertEquals(new ActivityRelation(D, C, REVERSED_CAUSALITY), response.get(17));
+        assertEquals(new ActivityRelation(D, D, CHOICE), response.get(18));
+        assertEquals(new ActivityRelation(D, E, REVERSED_CAUSALITY), response.get(19));
+
+        assertEquals(new ActivityRelation(E, A, REVERSED_CAUSALITY), response.get(20));
+        assertEquals(new ActivityRelation(E, B, CHOICE), response.get(21));
+        assertEquals(new ActivityRelation(E, C, CHOICE), response.get(22));
+        assertEquals(new ActivityRelation(E, D, CAUSALITY), response.get(23));
+        assertEquals(new ActivityRelation(E, E, CHOICE), response.get(24));
     }
 }
